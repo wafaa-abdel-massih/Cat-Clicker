30309339ca75b6dca59b1dcbd8299205dcf0f000
wafaa abdel massih <wafaa2016170498@cis.asu.edu.eg>
2023-11-22 01:08:04 +0200

    Update update.js

--- JS/update.js
+++ JS/update.js
@@ -1,1 +1,10 @@
-// something to make change 
+// something to make a change 
+
+var change_var;
+change_var = "test";
+
+// comment
+
+if (change_var!="anything"){
+  prite("it has test value in string data type")
+}
