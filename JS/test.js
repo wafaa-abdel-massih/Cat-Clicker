00180d3dda1c748b40a698ca267462ac3589ad67
wafaa abdel massih <wafaa2016170498@cis.asu.edu.eg>
2023-11-22 01:18:31 +0200

    Update test.js

--- JS/test.js
+++ JS/test.js
@@ -1,1 +1,9 @@
-// another change tp test again
+//Another change to test again
+
+var cat_test = "meow test";
+
+var len = cat_test.length;
+
+for (let i = 0; i < len; i++) {
+  console.log(cat_test + len + "<br>";
+}
